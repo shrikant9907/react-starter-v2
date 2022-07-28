0c266df00b596892df15983fd72b17fce6f338e6
Shrikant <shrikant9907@gmail.com>
2022-07-28 11:47:30 +0530

    build fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TopMenusRoutes } from './_config/TopMenus';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import SiteNavbar from './components/Navbar/Navbar';
+// import SiteNavbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <React.Fragment>
       <Router>
-        <SiteNavbar brandname="React Starter Logo" logotype="text" logosrc="" />
+        {/* <SiteNavbar brandname="React Starter Logo" logotype="text" logosrc="" /> */}
         <Routes>
           {/* Top Menu Routes Configuration is in _config/TopMenus.js */}
           {
